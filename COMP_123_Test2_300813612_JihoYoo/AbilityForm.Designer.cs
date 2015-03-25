e914cbf07c6bc6985292592c84274e11addb8dd6
JihoYoo <davidyoo214@gmail.com>
2015-03-25 09:59:59 -0400

    test2 -random ability

---
+++ COMP_123_Test2_300813612_JihoYoo/AbilityForm.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace COMP_123_Test2_300813612_JihoYoo
+{
+    partial class AbilityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AbilityGroupBox = new System.Windows.Forms.GroupBox();
+            this.PsycheTextBox = new System.Windows.Forms.TextBox();
+            this.PsycheLabel = new System.Windows.Forms.Label();
+            this.IntuitionTextBox = new System.Windows.Forms.TextBox();
+            this.IntuitionLabel = new System.Windows.Forms.Label();
+            this.ReasonTextBox = new System.Windows.Forms.TextBox();
+            this.ReasonLabel = new System.Windows.Forms.Label();
+            this.EnduranceTextBox = new System.Windows.Forms.TextBox();
+            this.EnduranceLabel = new System.Windows.Forms.Label();
+            this.StrengthTextBox = new System.Windows.Forms.TextBox();
+            this.StrengthLabel = new System.Windows.Forms.Label();
+            this.AgilityTextBox = new System.Windows.Forms.TextBox();
+            this.AgilityLabel = new System.Windows.Forms.Label();
+            this.FightingTextBox = new System.Windows.Forms.TextBox();
+            this.FightingLabel = new System.Windows.Forms.Label();
+            this.RollButton = new System.Windows.Forms.Button();
+            this.AbilityGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AbilityGroupBox
+            // 
+            this.AbilityGroupBox.Controls.Add(this.PsycheTextBox);
+            this.AbilityGroupBox.Controls.Add(this.PsycheLabel);
+            this.AbilityGroupBox.Controls.Add(this.IntuitionTextBox);
+            this.AbilityGroupBox.Controls.Add(this.IntuitionLabel);
+            this.AbilityGroupBox.Controls.Add(this.ReasonTextBox);
+            this.AbilityGroupBox.Controls.Add(this.ReasonLabel);
+            this.AbilityGroupBox.Controls.Add(this.EnduranceTextBox);
+            this.AbilityGroupBox.Controls.Add(this.EnduranceLabel);
+            this.AbilityGroupBox.Controls.Add(this.StrengthTextBox);
+            this.AbilityGroupBox.Controls.Add(this.StrengthLabel);
+            this.AbilityGroupBox.Controls.Add(this.AgilityTextBox);
+            this.AbilityGroupBox.Controls.Add(this.AgilityLabel);
+            this.AbilityGroupBox.Controls.Add(this.FightingTextBox);
+            this.AbilityGroupBox.Controls.Add(this.FightingLabel);
+            this.AbilityGroupBox.Location = new System.Drawing.Point(12, 38);
+            this.AbilityGroupBox.Name = "AbilityGroupBox";
+            this.AbilityGroupBox.Size = new System.Drawing.Size(280, 340);
+            this.AbilityGroupBox.TabIndex = 0;
+            this.AbilityGroupBox.TabStop = false;
+            this.AbilityGroupBox.Text = "Character Abilities";
+            // 
+            // PsycheTextBox
+            // 
+            this.PsycheTextBox.AllowDrop = true;
+            this.PsycheTextBox.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.PsycheTextBox.Location = new System.Drawing.Point(159, 241);
+            this.PsycheTextBox.Name = "PsycheTextBox";
+            this.PsycheTextBox.Size = new System.Drawing.Size(100, 29);
+            this.PsycheTextBox.TabIndex = 13;
+            this.PsycheTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.PsycheTextBox.TextChanged += new System.EventHandler(this.PsycheTextBox_TextChanged);
+            // 
+            // PsycheLabel
+            // 
+            this.PsycheLabel.AutoSize = true;
+            this.PsycheLabel.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.PsycheLabel.Location = new System.Drawing.Point(21, 251);
+            this.PsycheLabel.Name = "PsycheLabel";
+            this.PsycheLabel.Size = new System.Drawing.Size(76, 19);
+            this.PsycheLabel.TabIndex = 12;
+            this.PsycheLabel.Text = "PSYCHE";
+            this.PsycheLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.PsycheLabel.Click += new System.EventHandler(this.PsycheLabel_Click);
+            // 
+            // IntuitionTextBox
+            // 
+            this.IntuitionTextBox.AllowDrop = true;
+            this.IntuitionTextBox.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.IntuitionTextBox.Location = new System.Drawing.Point(159, 205);
+            this.IntuitionTextBox.Name = "IntuitionTextBox";
+            this.IntuitionTextBox.Size = new System.Drawing.Size(100, 29);
+            this.IntuitionTextBox.TabIndex = 11;
+            this.IntuitionTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.IntuitionTextBox.TextChanged += new System.EventHandler(this.IntuitionTextBox_TextChanged);
+            // 
+            // IntuitionLabel
+            // 
+            this.IntuitionLabel.AutoSize = true;
+            this.IntuitionLabel.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.IntuitionLabel.Location = new System.Drawing.Point(21, 215);
+            this.IntuitionLabel.Name = "IntuitionLabel";
+            this.IntuitionLabel.Size = new System.Drawing.Size(86, 19);
+            this.IntuitionLabel.TabIndex = 10;
+            this.IntuitionLabel.Text = "INTUITION";
+            this.IntuitionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.IntuitionLabel.Click += new System.EventHandler(this.IntuitionLabel_Click);
+            // 
+            // ReasonTextBox
+            // 
+            this.ReasonTextBox.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ReasonTextBox.Location = new System.Drawing.Point(159, 170);
+            this.ReasonTextBox.Name = "ReasonTextBox";
+            this.ReasonTextBox.Size = new System.Drawing.Size(100, 29);
+            this.ReasonTextBox.TabIndex = 9;
+            this.ReasonTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ReasonTextBox.TextChanged += new System.EventHandler(this.ReasonTextBox_TextChanged);
+            // 
+            // ReasonLabel
+            // 
+            this.ReasonLabel.AutoSize = true;
+            this.ReasonLabel.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ReasonLabel.Location = new System.Drawing.Point(21, 180);
+            this.ReasonLabel.Name = "ReasonLabel";
+            this.ReasonLabel.Size = new System.Drawing.Size(79, 19);
+            this.ReasonLabel.TabIndex = 8;
+            this.ReasonLabel.Text = "REASON";
+            this.ReasonLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ReasonLabel.Click += new System.EventHandler(this.ReasonLabel_Click);
+            // 
+            // EnduranceTextBox
+            // 
+            this.EnduranceTextBox.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.EnduranceTextBox.Location = new System.Drawing.Point(159, 135);
+            this.EnduranceTextBox.Name = "EnduranceTextBox";
+            this.EnduranceTextBox.Size = new System.Drawing.Size(100, 29);
+            this.EnduranceTextBox.TabIndex = 7;
+            this.EnduranceTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.EnduranceTextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // EnduranceLabel
+            // 
+            this.EnduranceLabel.AutoSize = true;
+            this.EnduranceLabel.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.EnduranceLabel.Location = new System.Drawing.Point(21, 145);
+            this.EnduranceLabel.Name = "EnduranceLabel";
+            this.EnduranceLabel.Size = new System.Drawing.Size(112, 19);
+            this.EnduranceLabel.TabIndex = 6;
+            this.EnduranceLabel.Text = "ENDURANCE";
+            this.EnduranceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.EnduranceLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // StrengthTextBox
+            // 
+            this.StrengthTextBox.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.StrengthTextBox.Location = new System.Drawing.Point(159, 100);
+            this.StrengthTextBox.Name = "StrengthTextBox";
+            this.StrengthTextBox.Size = new System.Drawing.Size(100, 29);
+            this.StrengthTextBox.TabIndex = 5;
+            this.StrengthTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.StrengthTextBox.TextChanged += new System.EventHandler(this.StrengthTextBox_TextChanged);
+            // 
+            // StrengthLabel
+            // 
+            this.StrengthLabel.AutoSize = true;
+            this.StrengthLabel.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.StrengthLabel.Location = new System.Drawing.Point(21, 110);
+            this.StrengthLabel.Name = "StrengthLabel";
+            this.StrengthLabel.Size = new System.Drawing.Size(97, 19);
+            this.StrengthLabel.TabIndex = 4;
+            this.StrengthLabel.Text = "STRENGTH";
+            this.StrengthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.StrengthLabel.Click += new System.EventHandler(this.StrengthLabel_Click);
+            // 
+            // AgilityTextBox
+            // 
+            this.AgilityTextBox.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.AgilityTextBox.Location = new System.Drawing.Point(159, 65);
+            this.AgilityTextBox.Name = "AgilityTextBox";
+            this.AgilityTextBox.Size = new System.Drawing.Size(100, 29);
+            this.AgilityTextBox.TabIndex = 3;
+            this.AgilityTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.AgilityTextBox.TextChanged += new System.EventHandler(this.AgilityTextBox_TextChanged);
+            // 
+            // AgilityLabel
+            // 
+            this.AgilityLabel.AutoSize = true;
+            this.AgilityLabel.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.AgilityLabel.Location = new System.Drawing.Point(21, 75);
+            this.AgilityLabel.Name = "AgilityLabel";
+            this.AgilityLabel.Size = new System.Drawing.Size(69, 19);
+            this.AgilityLabel.TabIndex = 2;
+            this.AgilityLabel.Text = "AGILITY";
+            this.AgilityLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AgilityLabel.Click += new System.EventHandler(this.AgilityLabel_Click);
+            // 
+            // FightingTextBox
+            // 
+            this.FightingTextBox.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FightingTextBox.Location = new System.Drawing.Point(159, 30);
+            this.FightingTextBox.Name = "FightingTextBox";
+            this.FightingTextBox.Size = new System.Drawing.Size(100, 29);
+            this.FightingTextBox.TabIndex = 1;
+            this.FightingTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.FightingTextBox.TextChanged += new System.EventHandler(this.FightingTextBox_TextChanged);
+            // 
+            // FightingLabel
+            // 
+            this.FightingLabel.AutoSize = true;
+            this.FightingLabel.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FightingLabel.Location = new System.Drawing.Point(21, 40);
+            this.FightingLabel.Name = "FightingLabel";
+            this.FightingLabel.Size = new System.Drawing.Size(81, 19);
+            this.FightingLabel.TabIndex = 0;
+            this.FightingLabel.Text = "FIGHTING";
+            this.FightingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // RollButton
+            // 
+            this.RollButton.Font = new System.Drawing.Font("Gulim", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.RollButton.Location = new System.Drawing.Point(100, 397);
+            this.RollButton.Name = "RollButton";
+            this.RollButton.Size = new System.Drawing.Size(100, 32);
+            this.RollButton.TabIndex = 1;
+            this.RollButton.Text = "Roll";
+            this.RollButton.UseVisualStyleBackColor = true;
+            this.RollButton.Click += new System.EventHandler(this.RollButton_Click);
+            // 
+            // AbilityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 441);
+            this.Controls.Add(this.RollButton);
+            this.Controls.Add(this.AbilityGroupBox);
+            this.Name = "AbilityForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AbilityForm";
+            this.AbilityGroupBox.ResumeLayout(false);
+            this.AbilityGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox AbilityGroupBox;
+        private System.Windows.Forms.TextBox IntuitionTextBox;
+        private System.Windows.Forms.Label IntuitionLabel;
+        private System.Windows.Forms.TextBox ReasonTextBox;
+        private System.Windows.Forms.Label ReasonLabel;
+        private System.Windows.Forms.TextBox EnduranceTextBox;
+        private System.Windows.Forms.Label EnduranceLabel;
+        private System.Windows.Forms.TextBox StrengthTextBox;
+        private System.Windows.Forms.Label StrengthLabel;
+        private System.Windows.Forms.TextBox AgilityTextBox;
+        private System.Windows.Forms.Label AgilityLabel;
+        private System.Windows.Forms.TextBox FightingTextBox;
+        private System.Windows.Forms.Label FightingLabel;
+        private System.Windows.Forms.TextBox PsycheTextBox;
+        private System.Windows.Forms.Label PsycheLabel;
+        private System.Windows.Forms.Button RollButton;
+    }
+}
+
---
+++ COMP_123_Test2_300813612_JihoYoo/AbilityForm.cs
@@ -0,0 +1,132 @@
+﻿﻿/* 
+ *  File Name: COMP_123_Test2_300813612_JihoYoo 
+ *  Author's name: Jiho Yoo 
+ *  Date : 3/25/2015 
+ *  App description : Randomly generates a role-­‐playing character’s ability scores. 
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP_123_Test2_300813612_JihoYoo
+{
+    public partial class AbilityForm : Form
+    {
+        Random rnd = new Random(); //Random Key
+
+        //Private instance valiables
+        private int fighting;
+        private int agililty;
+        private int strength;
+        private int endurance;       
+        private int reason;
+        private int intuition;
+        private int psyche;
+
+
+        //Constructor 
+        public AbilityForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Event Handler in the AbilityForm’s code.
+        private void RollButton_Click(object sender, EventArgs e)
+        {
+
+            //set up random numbers from 1 to 100.
+            fighting = rnd.Next(1, 101);
+            agililty = rnd.Next(1, 101);
+            strength = rnd.Next(1, 101);
+            endurance = rnd.Next(1, 101);
+            reason = rnd.Next(1, 101);
+            intuition = rnd.Next(1, 101);
+            psyche = rnd.Next(1, 101);
+
+            //TextBoxs convert to string for each valiables
+            FightingTextBox.Text = Convert.ToString(fighting);
+            AgilityTextBox.Text = Convert.ToString(agililty);
+            StrengthTextBox.Text = Convert.ToString(strength);
+            EnduranceTextBox.Text = Convert.ToString(endurance);
+            ReasonTextBox.Text = Convert.ToString(reason);
+            IntuitionTextBox.Text = Convert.ToString(intuition);
+            PsycheTextBox.Text = Convert.ToString(psyche);
+            
+            
+           
+
+        }
+
+        private void AgilityTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PsycheLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IntuitionTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IntuitionLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReasonTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReasonLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StrengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StrengthLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PsycheTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AgilityLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FightingTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
